f7f7e6b3d729391d68f19bd071b2539f84c9eb3a
Aeman Fatima <aeman098.fatima@gmail.com>
2024-06-23 15:41:06 +0500

    removed form module

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CarStepperComponent } from '../sell-car/car-stepper/car-stepper.component';
 import { CarSelectionComponent } from '../sell-car/car-selection/car-selection.component';
-import { FormComponent } from '../sell-car/form/form.component';
 
 const routes: Routes = [
   { path: '', component: CarSelectionComponent },
